99887703d2c0caca0cc97e79446fb857f4b06f07
450294533@qq.com <chenshuguang1234>
2018-01-23 21:03:09 +0800

    增加vo utils模块引入  增加POST调用服务端方法

--- spring-cloud-server-user/src/main/java/com/xxx/application/ComputeController.java
+++ spring-cloud-server-user/src/main/java/com/xxx/application/ComputeController.java
@@ -1,25 +1,26 @@
 package com.xxx.application;
 
+import com.alibaba.fastjson.JSON;
+import com.xxx.databasevo.user.User_userMes;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ComputeController {
     private final Logger logger = Logger.getLogger(getClass());
-    @Autowired
-    private DiscoveryClient client;
+//    @Autowired
+//    private DiscoveryClient client;
 
-    @RequestMapping(value = "/user_add", method = RequestMethod.GET)
-    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance instance = client.getLocalServiceInstance();
-        Integer r = a + b;
-        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
-        return 4;
+    @RequestMapping(value = "/user_add", method = RequestMethod.POST)
+
+    public String add( @RequestBody User_userMes userMes) {
+        String tel =userMes.getTel();
+//        ServiceInstance instance = client.getLocalServiceInstance();
+//        Integer r = a + b?
+//        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return JSON.toJSONString(userMes);
     }
 }
--- spring-cloud-web-ribbon-portal/src/main/java/com/xxx/application/ConsumerController.java
+++ spring-cloud-web-ribbon-portal/src/main/java/com/xxx/application/ConsumerController.java
@@ -1,10 +1,14 @@
 package com.xxx.application;
 
+import com.xxx.databasevo.user.User_userMes;
+import com.xxx.utils.DataUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.text.ParseException;
 
 @RestController
 public class ConsumerController {
@@ -13,8 +17,11 @@ public class ConsumerController {
 
 
     @RequestMapping(value = "/user_add", method = RequestMethod.GET)
-    public String user_add() {
-        return restTemplate.getForEntity("http://COMPUTE-SERVICE-USER/user_add?a=10&b=20", String.class).getBody();
+    public String user_add() throws ParseException {
+        User_userMes user_userMes =new User_userMes();
+        user_userMes.setTel("12345787");
+        String aa= DataUtils.dateToStamp("1970-01-06 11:45:55");
+        return restTemplate.postForEntity("http://COMPUTE-SERVICE-USER/user_add",user_userMes, String.class).getBody();
     }
     @RequestMapping(value = "/order_add", method = RequestMethod.GET)
     public String order_add() {
