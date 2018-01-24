b1c54fb9eaacf3f6b2702e89f29ec72ee7c55143
yichen <yichen-2@163.com>
2018-01-24 11:22:31 +0800

    pullrequest

--- spring-cloud-server-order/src/main/java/com/xxx/application/SpringCloudServerOrderApplication.java
+++ spring-cloud-server-order/src/main/java/com/xxx/application/SpringCloudServerOrderApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class SpringCloudServerOrderApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudServerOrderApplication.class, args);
 	}
